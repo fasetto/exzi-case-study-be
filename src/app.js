01faf3aaf2ef5f2e6f3a19025175bdbc89dc6222
Serkan <serkanbircan21@yandex.com>
2025-01-09 17:45:27 +0300

    initial

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import express from "express";
+
+import sseRouter from "./routes/sse.js";
+import usersRouter from "./routes/users.js";
+import walletsRouter from "./routes/wallets.js";
+import transferRouter from "./routes/transfer.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/sse", sseRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/wallets", walletsRouter);
+app.use("/api/transfer", transferRouter);
+
+app.use((req, res) => {
+  return res.sendStatus(404);
+});
+
+export default app;
